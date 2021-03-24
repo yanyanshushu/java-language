99d396c8f8074581ca268aa72b96db07f89fc249
sunqian <sunqianjsht@dangdang.com>
2021-03-24 23:34:12 +0800

    Arrays是浅拷贝

--- language-test/src/main/java/com/sunqian/Test.java
+++ language-test/src/main/java/com/sunqian/Test.java
@@ -1,6 +1,7 @@
 package com.sunqian;
 
 import java.net.MalformedURLException;
+import java.util.Arrays;
 
 public class Test {
 
@@ -9,7 +10,25 @@ public class Test {
      * @param args
      */
     public static void main(String[] args) throws MalformedURLException {
-
+        People[] o1 = {new People("sunqian")};
+        People[] o2 = Arrays.copyOf(o1, o1.length);
+        o1[0].setName("sunqianXXX");
+        System.out.println(o2[0].getName());
     }
 
+}
+class People{
+    String name;
+
+    People(String name){
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
